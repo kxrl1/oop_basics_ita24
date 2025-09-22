3598035e345e50de1e54bbe08f80049955c8da80
karl <karl.padu@voco.ee>
2025-09-22 15:14:06 +0300

    Add BMI calculation

--- 1._class_and_object/person.js
+++ 1._class_and_object/person.js
@@ -1,32 +1,21 @@
 class Person {
     #name = ""
     #age = 0
+    #weight = 0
+    #height = 0
 
     constructor(name) {
         this.#name = name
     }
+
+    // Vanus
 
     becomeOlder() {
         this.#age++
     }
 
     isAdult() {
-        if (this.#age < 18) {
-            return false
-        }
-        return true
-    }
-
-    printPerson() {
-        console.log(this.#name + ', age ' + this.#age + ' years')
-    }
-
-    setName() {
-        this.#name = name
-    }
-
-    getName() {
-        return this.#name
+        return this.#age >= 18
     }
 
     setAge(age) {
@@ -36,6 +25,70 @@ class Person {
     getAge() {
         return this.#age
     }
-} 
+
+    // Nimi
+
+    setName(name) {
+        this.#name = name
+    }
+
+    getName() {
+        return this.#name
+    }
+
+    // Kaal
+
+    setWeight(weight) {
+        if (weight > 0) {
+            this.#weight = weight
+        } else {
+            console.log("Kaal peab olema suurem kui 0!")
+        }
+    }
+
+    getWeight() {
+        return this.#weight
+    }
+
+    moreWeighted() {
+        this.#weight++
+    }
+
+    // Pikkus
+
+    setHeight(height) {
+        if (height > 0) {
+            this.#height = height
+        } else {
+            console.log("Pikkus peab olema suurem kui 0!")
+        }
+    }
+
+    getHeight() {
+        return this.#height
+    }
+
+    // BMI arvutamine
+
+    getBMI() {
+        if (this.#height > 0 && this.#weight > 0) {
+            let bmi = this.#weight / (this.#height * this.#height)
+            return Number(bmi.toFixed(2))
+        } else {
+            console.log("BMI arvutamiseks peab olema määratud positiivne kaal ja pikkus!")
+            return null
+        }
+    }
+
+    // Inimese info
+    
+    printPerson() {
+        console.log(
+            this.#name + ', age ' + this.#age + ' years, ' +
+            'weight: ' + this.#weight + ' kg, ' +
+            'height: ' + this.#height + ' m'
+        )
+    }
+}
 
 module.exports = Person
--- 1._class_and_object/persons.js
+++ 1._class_and_object/persons.js
@@ -3,25 +3,28 @@ const bob = new Person('Bob')
 const brian = new Person('Brian')
 const alice = new Person('Alice')
 
+bob.setAge(15)
+bob.setWeight(60)
+bob.setHeight(1.7)
+
+brian.setAge(20)
+brian.setWeight(80)
+brian.setHeight(1.8)
+
+alice.setAge(17)
+alice.setWeight(50)
+alice.setHeight(1.6)
+
 const persons = [bob, brian, alice]
 
-while (bob.getAge() < 15)  {
-    bob.becomeOlder()
-}
-
-for (let i = 0; i < 20; i++) {
-    brian.becomeOlder()
-}
-
-bob.printPerson()
-brian.printPerson()
-alice.printPerson()
-
 persons.forEach(person => {
+    person.printPerson()
+    console.log("BMI: " + person.getBMI())
     if (person.isAdult()) {
         console.log(person.getName() + ' is adult')    
     } else {
         console.log(person.getName() + ' is not adult')
     }
+    console.log("------")
 })
 
