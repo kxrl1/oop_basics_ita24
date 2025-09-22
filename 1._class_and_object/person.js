55124f0c9d25fa040f1ba353c0bc4ea5f4585d89
karl <karl.padu@voco.ee>
2025-09-22 13:56:35 +0300

    Add more methods into the class

--- 1._class_and_object/person.js
+++ 1._class_and_object/person.js
@@ -6,6 +6,21 @@ class Person {
         this.#name = name
     }
 
+    becomeOlder() {
+        this.#age++
+    }
+
+    isAdult() {
+        if (this.#age < 18) {
+            return false
+        }
+        return true
+    }
+
+    printPerson() {
+        console.log(this.#name + ', age ' + this.#age + ' years')
+    }
+
     setName() {
         this.#name = name
     }
